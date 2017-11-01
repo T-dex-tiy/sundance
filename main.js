b74a5155b68c45a253fef202e9f06b1dcb3b363b
T-Dex <dex.m.mills@gmail.com>
2017-11-01 16:36:26 -0600

    Add css and Js style

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const main = document.getElementById('main');
 
 function shrink() {
   console.log('Wee');
+  const test = document.querySelector('.main');
+  test.classList.add('colorchange');
 }
 
 main.addEventListener('keydown', shrink);
